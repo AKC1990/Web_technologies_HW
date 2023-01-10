f83a493af2bf3116b7a2464bcd58266fa4ce5a59
Kirill Antropov <chs4t-740@mail.ru>
2023-01-10 09:46:28 +0300

    Added Task 2 in HW_3

--- HW_3/1.js
+++ HW_3/1.js
@@ -2,8 +2,6 @@
 // const temperature_in_celsius = parseFloat(prompt("Укажите температуру в градусах Цельсия"));
 // alert(`Цельсий: ${temperature_in_celsius}, Фаренгейт: ${((9 / 5) * temperature_in_celsius + 32).toFixed(1)}`)
 
-
-
 /* Решение через функцию */
 function getTempInF(temperature) {
     return `Цельсий: ${temperature}, Фаренгейт: ${((9 / 5) * temperature + 32).toFixed(1)}`
